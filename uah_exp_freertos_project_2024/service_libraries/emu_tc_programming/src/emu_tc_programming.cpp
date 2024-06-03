541f805b4a2a5d5d44ff2e1f212eb576c7f9c264
atcsol <atcsol@ubuntu>
2024-06-03 21:54:49 +0200

    ProyectoFinal2

--- uah_exp_freertos_project_2024/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
+++ uah_exp_freertos_project_2024/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
@@ -10,14 +10,61 @@
 #include <public/emu_gss_v1.h>
 
 
-#define FT_UAH_EXP_SERV_129_00XX
+//#define FT_UAH_EXP_SERV_129_0010
+//#define FT_UAH_EXP_SERV_129_0020
 
-//TODO
 
+
+#ifdef FT_UAH_EXP_SERV_129_0010
+
+// Definir los tiempos para cada paso del test
+#define FT_129_0010_TIME_step0 5
+#define FT_129_0010_TIME_step1 10
+//#define FT_129_0010_TIME_step2 15
+
+// Paso 0: Configurar la estructura de HK para reportar CVx y CVy cada 5s
+EmuGSS_TCProgram3_31 prog_FT_129_0010_step_0(FT_129_0010_TIME_step0,
+    "FT_SOLO_EPD_ICU_SERV_129_0010 step 0, Config HK to report CVx and CVy every 5s",
+    10, 5);
+
+// Paso 1: Fijar el valor de las velocidades CVx y CVy
+EmuGSS_TCProgram129_1 prog_FT_129_0010_step_1(FT_129_0010_TIME_step1,
+    "FT_SOLO_EPD_ICU_SERV_129_0010 step 1, Set CVx and CVy values",
+    -0.2, 0.2);
+
+
+#endif
+
+#ifdef FT_UAH_EXP_SERV_129_0020
+
+
+// Definir los tiempos para cada paso del test
+#define FT_129_0020_TIME_step0 5
+#define FT_129_0020_TIME_step1 10
+//#define FT_129_0020_TIME_step2 15
+
+// Paso 0: Configurar la estructura de HK para reportar Kpx y Kpy cada 5s
+EmuGSS_TCProgram3_31 prog_FT_129_0020_step_0(FT_129_0020_TIME_step0,
+    "FT_SOLO_EPD_ICU_SERV_129_0020 step 0, Config HK to report Kpx and Kpy every 5s",
+    10, 5);
+
+// Paso 1: Fijar el valor de las constantes Kpx y Kpy
+EmuGSS_TCProgram129_2 prog_FT_129_0020_step_1(FT_129_0020_TIME_step1,
+    "FT_SOLO_EPD_ICU_SERV_129_0020 step 1, Set Kpx and Kpy values",
+    0.3, 0.3);
+
+
+#endif
+
+
+//cambiar valores de velocidades y kas
+
+// formato
+/*
 
 #ifdef FT_UAH_EXP_SERV_129_00XX
 
-/*
+
 EmuGSS_TCProgram129_1 prog_FT_00XX_step_0(UNITIME_AFTER_POWER_ON + X,
 					"FT_UAH_EXP_SERV_129_00XX step 0, Set Kp", 0.2, 0.2);
 
@@ -25,8 +72,10 @@ EmuGSS_TCProgram129_2 prog_FT_00XX_step_0(UNITIME_AFTER_POWER_ON + X,
 					"FT_UAH_EXP_SERV_129_00XX step 0, Set Command Vx and Vy "
 					, XXXX, XXXXX);
 
-*/
+
 
 #endif
+
+*/
 
 
